6fd5560c701554e491fb09c83960ec9f1f47d947
Duong <duongptryu@gmail.com>
2021-10-01 09:26:07 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'native_day4',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
